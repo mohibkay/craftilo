19fa42a9a9ce4d2cb997acf93f5c843945718c68
Mohib Khan <mohibk.in@gmail.com>
2021-06-18 10:45:10 +0530

    Added custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      maroon: "#db4c3f",
+      white: "#ffffff",
+      offWhite: "#fafafa",
+    },
     extend: {},
   },
   variants: {
