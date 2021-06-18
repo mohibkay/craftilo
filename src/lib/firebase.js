53f530fbf403588c8d4fa8937672d1d3aea304ca
Mohib Khan <mohibk.in@gmail.com>
2021-06-18 10:43:53 +0530

    Added firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDS4GKJ4vzXs1jyht7T45SDLrsRaqZA6sQ",
+  authDomain: "mohib-todoist.firebaseapp.com",
+  projectId: "mohib-todoist",
+  storageBucket: "mohib-todoist.appspot.com",
+  messagingSenderId: "691824805329",
+  appId: "1:691824805329:web:1b5814e1969e87af878cf0",
+  measurementId: "G-LFJB64YWFW",
+};
+
+const firebase = Firebase.initializeApp(firebaseConfig);
+
+export default firebase;
