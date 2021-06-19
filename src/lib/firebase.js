8e11d1025833dbe9a5af5024b87068ec6e294311
Mohib Khan <mohibk.in@gmail.com>
2021-06-19 11:17:43 +0530

    chore: move firebase config to env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,33 +1,14 @@
-// import app from "firebase/app";
-// import "firebase/firestore";
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDS4GKJ4vzXs1jyht7T45SDLrsRaqZA6sQ",
-//   authDomain: "mohib-todoist.firebaseapp.com",
-//   projectId: "mohib-todoist",
-//   storageBucket: "mohib-todoist.appspot.com",
-//   messagingSenderId: "691824805329",
-//   appId: "1:691824805329:web:1b5814e1969e87af878cf0",
-//   measurementId: "G-LFJB64YWFW",
-// };
-
-// const firebase = app.initializeApp(firebaseConfig);
-
-// export default firebase;
-
 import Firebase from "firebase/app";
 import "firebase/firestore";
 // import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyDS4GKJ4vzXs1jyht7T45SDLrsRaqZA6sQ",
-  authDomain: "mohib-todoist.firebaseapp.com",
-  projectId: "mohib-todoist",
-  storageBucket: "mohib-todoist.appspot.com",
-  messagingSenderId: "691824805329",
-  appId: "1:691824805329:web:1b5814e1969e87af878cf0",
-  measurementId: "G-LFJB64YWFW",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebase = Firebase.initializeApp(config);
