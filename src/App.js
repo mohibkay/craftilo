51bca561291fbbb554f119f8c630cf953f20fbab
Mohib Khan <mohibk.in@gmail.com>
2021-07-02 11:26:47 +0530

    feat: lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
 import { ROUTES } from "./constants";
 import "./App.css";
+import Loader from "./utils/Loader";
 
-import Dashboard from "./pages/Dashboard";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import ProtectedAuth from "./components/auth/ProtectedAuth";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
         <Router>
-          <Switch>
-            <ProtectedRoute
-              exact
-              path={ROUTES.DASHBOARD}
-              component={Dashboard}
-            />
-            <ProtectedAuth path={ROUTES.SIGN_UP} component={SignUp} />
-            <ProtectedAuth path={ROUTES.LOGIN} component={Login} />
-          </Switch>
+          <Suspense fallback={<Loader />}>
+            <Switch>
+              <ProtectedRoute
+                exact
+                path={ROUTES.DASHBOARD}
+                component={Dashboard}
+              />
+              <ProtectedAuth path={ROUTES.SIGN_UP} component={SignUp} />
+              <ProtectedAuth path={ROUTES.LOGIN} component={Login} />
+            </Switch>
+          </Suspense>
         </Router>
       </ProjectsProvider>
     </SelectedProjectProvider>
