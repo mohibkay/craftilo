65167e9eda2d8b68d26badf1f5c0d48288c995ee
Mohib Khan <mohibk.in@gmail.com>
2021-10-30 11:31:10 +0530

    style: icon color

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -84,7 +84,7 @@ const Tasks = ({ showSidebar, setShowSidebar }) => {
             {showArchivedList ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="h-6 w-6 text-gray-base"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -99,7 +99,7 @@ const Tasks = ({ showSidebar, setShowSidebar }) => {
             ) : (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="h-6 w-6 text-gray-base"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
