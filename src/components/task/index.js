0b84ed1e174b7eb5796f057933f320a96f191c1f
Mohib Khan <mohibk.in@gmail.com>
2021-10-30 10:27:31 +0530

    fix: null check for archived tasks

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -73,7 +73,7 @@ const Tasks = ({ showSidebar, setShowSidebar }) => {
 
       <AddTask />
 
-      {archivedTasks.length ? (
+      {archivedTasks?.length ? (
         <>
           {" "}
           <div
